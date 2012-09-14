1544265b7b76fd0e959b864b9897efa4c091ec29
Ben Ford <bentford@gmail.com>
2012-09-14 14:45:55 -0700

    Added better formatting to the note text.

--- pivotal_bridge.php
+++ pivotal_bridge.php
@@ -1,22 +1,25 @@
 <?php
 
+$PROJECT_ID = '626107';
+$TOKEN = '55fe2b03cbb9c0c17e8072b1a217c752';
 
-print($_POST);
-
-$PROJECT_ID = '5';
-$TOKEN = 'token';
+$UNFUDDLE_SUBDOMAIN = 'theprogram';
+$UNFUDDLE_REPO_ID = '90';
 
 $request_xml = (isset($HTTP_RAW_POST_DATA)) ? $HTTP_RAW_POST_DATA : file_get_contents('php://input');
 $xml = simplexml_load_string($request_xml); 
-$revisionNumber = $xml->message;
+
 
 $matches = array();
-preg_match('/story\s*:\s*(\d*)/i', $xml->message, &$matches);
+preg_match('/story\s*:\s*(\d*)(.*)/i', $xml->message, $matches);
 
-$storyId =  $matches[0];
+$storyId =  $matches[1];
+$notes = $matches[2];
 
-$url = 'http://www.pivotaltracker.com/services/v3/projects/'.$PROJECT_ID.'/stories/'.$storyId.'/notes';
-$xml = '<note><text>Commit: '.$xml->revision.'</text></note>';
+$url = 'http://www.pivotaltracker.com/services/v4/projects/'.$PROJECT_ID.'/stories/'.$storyId.'/comments';
+$xml = '<comment><text>'."Commit Notes:\n".$notes."\n\nCommit URL:\nhttp://".$UNFUDDLE_SUBDOMAIN.'.unfuddle.com/a#/repositories/'.$UNFUDDLE_REPO_ID.'/commit?commit='.$xml->revision.'</text></comment>';
+
+print 'posting to url: '.$url."\n";
 
 $ch = curl_init( $url );
 curl_setopt( $ch, CURLOPT_POST, 1);
